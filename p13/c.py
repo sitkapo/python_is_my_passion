104836ed1424fb565e01243abffe55b44ebd2089
Piotr Ponikiewski <p.ponikiewski7@gmail.com>
2022-02-10 14:58:56 +0100

    next commit

--- p13/c.py
+++ p13/c.py
@@ -1,21 +1,36 @@
-# def dominoes(str, list:str):
+# # def dominoes(str, list:str):
 
-#     list.sort()
-#     for i in list:
+# #     list.sort()
+# #     for i in list:
         
         
 
 
-# dominoes('A',['AA','AC','BC','CA'])
+# # dominoes('A',['AA','AC','BC','CA'])
 
-list=['AA','AC','BC','CA']
-# print(list[1])
-index =0
-for i in range(len(list)):
-    index+=1
-    # print(index,list[i])
-    for i in list[i]:
-        print(index,i)
+# list=['AA','AC','BC','CA']
+# # print(list[1])
+# index =0
+# for i in range(len(list)):
+#     index+=1
+#     # print(index,list[i])
+#     for i in list[i]:
+#         print(index,i)
     
-    # for i in list[i]:
-    #     print(i)
+# print(index)
+#     # for i in list[i]:
+#     #     print(i)
+
+
+def dominoes(str, list:str):
+    index =0
+    for i in range(len(list)):
+        index+=1
+        # print(index,list[i])
+        for i in list[i]:
+            print(index,i)
+            
+
+
+
+dominoes('A',['AA','AC','BC','CA'])
